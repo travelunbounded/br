42554c90b03a918bf3a91028d2b1ab74003107e4
Travel Unbounded <travelunbounded@gmail.com>
2015-09-26 18:08:10 +0530

    v0.1

---
+++ index.js
@@ -0,0 +1,51 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+var redis = require('redis');
+
+mongoose = require('mongoose');
+var uri = 'mongodb://bradmin:abcd1234@ds051863.mongolab.com:51863/br';
+db = mongoose.connect(uri);
+Schema = mongoose.Schema;
+
+  var userSchema = new Schema({
+      name  :  { type: String, default: '' }
+    , password   :  { type: String, default: '' }
+  });
+  var userModel = mongoose.model('User', userSchema);
+  var test = new userModel({name: "test", password: "test"})
+
+  console.log("me: " + test)
+
+  test.save(function (err, test) {
+    console.log("saved?")
+    if (err) {
+      console.log("error");
+      return console.error(err);
+    }
+    console.log("saved!")
+  });
+  console.log("after save");
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
